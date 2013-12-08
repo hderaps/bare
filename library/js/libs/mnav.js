18ab35f661bb896d93de99d574adec650a23320a
Karl Castillo <khcastillo@hotmail.com>
2013-12-07 16:12:54 -0800

    updated mnav

--- library/js/libs/mnav.js
+++ library/js/libs/mnav.js
@@ -14,7 +14,7 @@
             var mobileContainer = $('<div/>', {
                 class: "mnav-mobile-btn"
             });
-            base.$elem.addClass('mnav-theme');
+            base.$elem.addClass(base.options.theme);
             base.$elem.prepend(mobileContainer);
             
             if(base.options.mobileButtonPos === 'left') {
